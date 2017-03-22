e95690b9c68cbd9c33f87eeb111cd2f26c322472
Saveliy Prokopenko <saveliy.prokopenko@icloud.com>
2017-03-22 18:11:25 +0200

    removed Kitura dependency and added direct CCurl and SwiftyJSON instead

--- Package.swift
+++ Package.swift
@@ -3,7 +3,9 @@ import PackageDescription
 let package = Package(
     name: "telegram-bot-swift",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6),
+        //.Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6),
+	.Package(url: "https://github.com/IBM-Swift/CCurl.git", majorVersion: 0, minor: 2),
+	.Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 15),
         .Package(url: "https://github.com/smud/ScannerUtils.git", majorVersion: 1)
     ]
 )
